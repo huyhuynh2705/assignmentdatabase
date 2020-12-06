f782b84cbe3632d6028e9ae33d4dab14c47138b6
huyhuynh2705 <67199597+huyhuynh2705@users.noreply.github.com>
2020-12-06 10:22:25 +0700

    Update DataRequirementsOfEachUserGroup.sql

--- DataRequirementsOfEachUserGroup.sql
+++ DataRequirementsOfEachUserGroup.sql
@@ -1,39 +1,40 @@
-﻿
+USE PUBLICATION;
 --(i). Ban biên tập
 --(i.1). Cập nhật phân công phản biện cho một bài báo.
-CREATE PROCEDURE updateAssign (@articleId INT, @reviewerSsn INT, @editorialBoardSsn INT, @assignDate DATE)
+CREATE PROCEDURE updateAssign (@articleId VARCHAR(15), @reviewerSsn VARCHAR(15), @editorialBoardSsn VARCHAR(15), @assignDate DATE)
 AS
 BEGIN
 	IF EXISTS(select * from ASSIGN where ArticleID=@articleId)
 	   update ASSIGN set ReviewerSsn=@reviewerSsn, EditorialBoardSsn=@editorialBoardSsn, AssignDate=@assignDate 
 	   where ArticleID=@articleId
 END
+GO
 select * from ASSIGN
-EXEC updateAssign 2,4,1,'2020-11-20'  
+EXEC updateAssign 412345,3012345, 2012345, '2020-11-20'
 --------------------------------------------------------------------------------------------
 --(i.2). Cập nhật trạng thái xử lý cho một bài báo: 
 -- phản biện = 0, phản hồi phản biện = 1, hoàn tất phản biện = 2, xuất bản = 3, đã đăng = 4.
-CREATE PROCEDURE updateStatusArticle (@status INT, @id INT)
+CREATE PROCEDURE updateStatusArticle (@status INT, @id VARCHAR(15))
 AS
 BEGIN
 	UPDATE ARTICLE SET Status = @status WHERE ID=@id;
 END
+GO
 select * from ARTICLE
-EXEC updateStatusArticle 1,3  
-
+EXEC updateStatusArticle 1,412345  
 ---------------------------------------------------------------------------------------------
 --(i.3). Cập nhật kết quả sau phản biện cho một bài báo.
 --rejection = 0, minor revision = 1, major revision = 2, acceptance = 3.
-CREATE PROCEDURE UpdateResult @ID VARCHAR(15), @Result INT
+CREATE PROCEDURE UpdateResult (@ID VARCHAR(15), @Result INT)
 AS 
-UPDATE ARTICLE SET Result = @Result WHERE ID = @ID;
-
-EXEC UpdateResult 0, 1;
-EXEC UpdateResult 0, 2;
+	UPDATE ARTICLE SET Result = @Result WHERE ID = @ID;
+GO
+select * from ARTICLE
+EXEC UpdateResult 412345,2
 ----------------------------------------------------------------------------------------------
 --(i.4). Cập nhật kết quả sau hoàn tất phản biện cho một bài báo.
-EXEC UpdateResult 0, 0;
-EXEC UpdateResult 0, 3;
+select * from ARTICLE
+EXEC UpdateResult 412345,3
 ----------------------------------------------------------------------------------------------
 --i.5.0 Xem danh sách các bài báo theo mỗi loại (nghiên cứu, phản biện sách, tổng quan) hoặc toàn bộ theo mỗi trạng thái xử lý
 CREATE PROCEDURE ViewArticleByStatus (@typeArticle VARCHAR(255), @status INT)
@@ -48,8 +49,12 @@ BEGIN
 	IF @typeArticle = 'ARTICLE'
 		SELECT * FROM ARTICLE WHERE ID IN (SELECT ID FROM ARTICLE WHERE Status = @status) 
 END
-EXEC ViewArticleByStatus 'REVIEW_BOOK', 0
-
+GO
+select * from ARTICLE
+EXEC ViewArticleByStatus 'ARTICLE', 1
+select * from ARTICLE
+select * from RESEARCH
+EXEC ViewArticleByStatus 'RESEARCH', 2
 ----------------------------------------------------------------------------------------------
 --(i.5). Xem danh sách các bài báo theo mỗi loại (nghiên cứu, phản biện sách, tổng quan) chưa được xử lý phản biện.
 -- FIX
@@ -73,10 +78,16 @@ BEGIN
 	IF @typeArticle = 'ARTICLE'
 		SELECT * FROM ARTICLE WHERE ID IN (SELECT ID FROM ARTICLE WHERE Status = @status AND SentDate > DATEADD(year,-@n,GETDATE()))
 END
-EXEC ViewArticleByTypeByYear 'REVIEW_BOOK', 3, 3
+GO
+select * from ARTICLE
+EXEC ViewArticleByTypeByYear 'ARTICLE', 3, 10
 ------------------------------------------------------------------------------------------------------------------
 --(i.7). Xem danh sách các bài báo đã đăng theo mỗi loại (nghiên cứu, phản biện sách, tổng quan) trong 3 năm gần nhất.
-EXEC ViewArticleByTypeByYear 'REVIEW_BOOK', 3, 3
+EXEC ViewArticleByTypeByYear 'ARTICLE', 0, 10
+EXEC ViewArticleByTypeByYear 'ARTICLE', 1, 10
+EXEC ViewArticleByTypeByYear 'ARTICLE', 2, 10
+EXEC ViewArticleByTypeByYear 'ARTICLE', 3, 10
+EXEC ViewArticleByTypeByYear 'ARTICLE', 4, 10
 -----------------------------------------------------------------------------------------------
 -- i.8.0 Xem danh sách các bài báo theo mỗi loại (nghiên cứu, phản biện sách, tổng quan) hoặc toàn bộ theo mỗi trạng thái xử lý của một tác giả 
 CREATE PROCEDURE ViewArticleByTypeByAuthor (@typeArticle VARCHAR(255), @status INT, @authorSsn INT)
@@ -91,13 +102,15 @@ BEGIN
 	IF @typeArticle = 'ARTICLE'
 		SELECT * FROM ARTICLE WHERE ID IN (SELECT ID FROM ARTICLE WHERE Status = @status AND AuthorSSn = @authorSsn)
 END
-EXEC ViewArticleByTypeByYear 'ARTICLE', 3, 0
+GO
+select * from AUTHOR
+EXEC ViewArticleByTypeByAuthor 'ARTICLE', 3, 1012345
 -----------------------------------------------------------------------------------------------
 --(i.8). Xem danh sách các bài báo được xuất bản của một tác giả.
-EXEC ViewArticleByTypeByYear 'ARTICLE', 3, 0
+EXEC ViewArticleByTypeByAuthor 'ARTICLE', 3, 1012345
 -----------------------------------------------------------------------------------------------
 --(i.9). Xem danh sách các bài báo đã đăng của một tác giả.
-EXEC ViewArticleByTypeByYear 'ARTICLE', 4, 0
+EXEC ViewArticleByTypeByAuthor 'ARTICLE', 4, 1012345
 ------------------------------------------------------------------------------------------------
 --i.10.0 Xem tổng số bài báo theo mỗi loại hoặc toàn bộ theo trạng thái xử lý
 CREATE PROCEDURE CountArticleByType(@typeArticle VARCHAR(255), @status INT)
@@ -112,17 +125,17 @@ BEGIN
 	IF @typeArticle = 'ARTICLE'
 		SELECT COUNT(*) as countArticle FROM ARTICLE WHERE ID IN (SELECT ID FROM ARTICLE WHERE Status = @status)
 END
-EXEC CountArticleByType 'ARTICLE', 0
+GO
 -----------------------------------------------------------------------------------------------
 --(i.10). Xem tổng số bài báo đang được phản biện.
+select * from article
 EXEC CountArticleByType 'ARTICLE', 0
 -----------------------------------------------------------------------------------------------
 --(i.11). Xem tổng số bài báo đang được phản hồi phản biện.
 EXEC CountArticleByType 'ARTICLE', 1
 -----------------------------------------------------------------------------------------------
 --(i.12). Xem tổng số bài báo đang được xuất bản.
-EXEC CountArticleByType 'ARTICLE', 3
-
+EXEC CountArticleByType 'ARTICLE', 4
 -------------------------------------------------------------------------------------------------------------------------------------------------
 
 --(ii). Phản biện
@@ -134,19 +147,23 @@ BEGIN
 	   UPDATE SCIENTIST set Email = @Email, Job = @Job, Fname = @Fname, LName = @LName, Address = @Address, WorkPlace = @WorkPlace
 	   WHERE Ssn = @Ssn
 END
-SELECT * FROM SCIENTIST
-EXEC UpdateInformation @Ssn = 111111111, @Email = 'abc@gmmail.com', @Job='Teacher', @Fname='Tom', @LName = 'Cruise', @Address = '221B Baker .St', @WorkPlace = 'London' ;
+GO
+select * from SCIENTIST
+EXEC UpdateInformation @Ssn = 3012345, @Email = 'abc@gmmail.com', @Job='Teacher', @Fname='Tom', @LName = 'Cruise', @Address = '221B Baker .St', @WorkPlace = 'London' ;
 --------------------------------------------------------------------------------------------
 
 --(ii.2). Cập nhật phản biện cho một bài báo.
-CREATE PROCEDURE UpdateCriteriaContent (@ID VARCHAR(15), @ReviewContent TEXT)
+CREATE PROCEDURE UpdateCriteriaReviewer (@ID VARCHAR(15), @ReviewerSsn VARCHAR(15))
 AS
 BEGIN
 	IF EXISTS(SELECT * FROM EVALUATE WHERE ArticleID = @ID)
-		UPDATE EVALUATE SET ReviewContent = @ReviewContent WHERE ArticleID = @ID;
+		UPDATE EVALUATE SET ReviewerSsn = @ReviewerSsn WHERE ArticleID = @ID;
 END
 SELECT * FROM EVALUATE
-EXEC UpdateCriteriaContent 1,'abcabcabcabc';
+GO
+select * from REVIEWER
+select * from EVALUATE
+EXEC UpdateCriteriaReviewer 412345, 3012347;
 ---------------------------------------------------------------------------------------------
 
 --(ii.3). Xem danh sách các bài báo theo mỗi loại (nghiên cứu, phản biện sách, tổng quan) mà mình đang phản biện.
@@ -162,7 +179,11 @@ BEGIN
 	IF @typeArticle = 'ARTICLE'
 		SELECT * FROM ASSIGN JOIN ARTICLE ON ArticleID = ID WHERE ReviewerSsn = @ReviewerSsn AND Status IN (0,1) AND ID IN (SELECT ID FROM ARTICLE)
 END
-EXEC ViewArticleReviewing 'RESEARCH', 111111111;
+GO
+select * from ASSIGN
+EXEC ViewArticleReviewing 'OVERVIEW', 3012345;
+select * from ASSIGN
+select * from EVALUATE
 ---------------------------------------------------------------------------------------------
 
 --(ii.4). Xem danh sách các bài báo theo mỗi loại (nghiên cứu, phản biện sách, tổng quan) mà mình đã phản biện trong 3 năm gần đây nhất.
@@ -178,21 +199,45 @@ BEGIN
 	IF @typeArticle = 'ARTICLE'
 		SELECT * FROM ASSIGN JOIN ARTICLE ON ArticleID = ID WHERE ReviewerSsn = @ReviewerSsn AND Status = 2 AND AssignDate > DATEADD(year,-3,GETDATE()) AND ID IN (SELECT ID FROM ARTICLE)
 END
-EXEC ViewArticleReviewedIn3Y 'RESEARCH', 111111111;
+GO
+EXEC ViewArticleReviewedIn3Y 'OVERVIEW', 3012345;
 ---------------------------------------------------------------------------------------------
 
 --(ii.5). Xem danh sách các bài báo của một tác giả mà mình đang phản biện.
-CREATE PROCEDURE ViewArticleOfAuthorReviewing (@ReviewerSsn VARCHAR (15))
+CREATE PROCEDURE ViewArticleOfAuthorReviewing (@ReviewerSsn VARCHAR (15), @AuthorSsn VARCHAR (15))
 AS
-SELECT * FROM ARTICLE WHERE AuthorSSn IN (SELECT AuthorSSn FROM ASSIGN JOIN ARTICLE ON ArticleID = ID WHERE ReviewerSsn = @ReviewerSsn AND Status IN (0,2))
-EXEC ViewArticleOfAuthorReviewing 111111111;
+	SELECT * 
+	FROM (SELECT ArticleID 
+		  FROM ASSIGN JOIN ARTICLE ON ArticleID = ID
+		  WHERE ReviewerSsn = @ReviewerSsn AND Status = 0) as d 
+		  JOIN 
+		  (SELECT ID 
+		  FROM WRITE 
+		  WHERE Ssn = @AuthorSsn) as dd 
+		  ON d.ArticleID = dd.ID
+GO
+	
+select * from ASSIGN
+select * from WRITE
+select * from ARTICLE
+EXEC ViewArticleOfAuthorReviewing 3012346, 1012346;
 ---------------------------------------------------------------------------------------------
 
 --(ii.6). Xem danh sách các bài báo của một tác giả mà mình đã phản biện trong 3 năm gần đây nhất.
-CREATE PROCEDURE ViewArticleOfAuthorReviewingIn3Y (@ReviewerSsn VARCHAR (15))
+CREATE PROCEDURE ViewArticleOfAuthorReviewingIn3Y (@ReviewerSsn VARCHAR (15), @AuthorSsn VARCHAR (15))
 AS
-SELECT * FROM ARTICLE WHERE AuthorSSn IN (SELECT AuthorSSn FROM ASSIGN JOIN ARTICLE ON ArticleID = ID WHERE ReviewerSsn = @ReviewerSsn AND Status = 2 AND AssignDate > DATEADD(year,-3,GETDATE()))
-EXEC ViewArticleOfAuthorReviewingIn3Y 111111111;
+	SELECT * 
+	FROM (SELECT ArticleID 
+		  FROM ASSIGN JOIN ARTICLE ON ArticleID = ID
+		  WHERE ReviewerSsn = @ReviewerSsn AND Status = 0 AND AssignDate > DATEADD(year,-3,GETDATE())) as d 
+		  JOIN 
+		  (SELECT ID 
+		  FROM WRITE 
+		  WHERE Ssn = @AuthorSsn) as dd 
+		  ON d.ArticleID = dd.ID
+GO
+drop proc ViewArticleOfAuthorReviewingIn3Y
+EXEC ViewArticleOfAuthorReviewingIn3Y 3012346, 1012345;
 ---------------------------------------------------------------------------------------------
 
 --(ii.7). Xem danh sách tác giả có nhiều bài báo nhất mà mình đã phản biện.
@@ -202,7 +247,7 @@ BEGIN
 	WITH numArticleAndAuthorReviewed
 	AS
 	(
-		SELECT WRITE.Ssn ,COUNT(*)NumOfArtile
+		SELECT WRITE.Ssn ,COUNT(*)NumOfArticle
 		FROM ASSIGN JOIN WRITE ON ASSIGN.ArticleID= WRITE.ID
 		WHERE ASSIGN.ReviewerSsn=@reviewssn
 		GROUP BY WRITE.Ssn
@@ -210,18 +255,20 @@ BEGIN
 
 	SELECT Ssn, NumOfArtile
 	FROM numArticleAndAuthorReviewed
-	WHERE NumOfArtile IN (SELECT MAX(NumOfArtile)
+	WHERE NumOfArticle IN (SELECT MAX(NumOfArticle)
 						  FROM numArticleAndAuthorReviewed)
 END
-EXEC getXListTopAuthorReviewed @reviewssn='1';
-
+GO
+select * from ASSIGN JOIN WRITE ON ArticleID = ID ORDER BY ReviewerSsn
+EXEC getXListTopAuthorReviewed 3012346;
 ---------------------------------------------------------------------------------------------
 
 --(ii.8). Xem kết quả phản biện của các bài báo mà mình đã phản biện trong năm nay.
 CREATE PROCEDURE ViewResultArticleReviewed (@ReviewerSsn VARCHAR (15))
 AS
-SELECT Result FROM EVALUATE JOIN ARTICLE ON ID = ArticleID WHERE ReviewerSsn = @ReviewerSsn
-EXEC ViewResultArticleReviewed 111111111;
+SELECT Result,ID FROM EVALUATE JOIN ARTICLE ON ID = ArticleID WHERE ReviewerSsn = @ReviewerSsn
+GO
+EXEC ViewResultArticleReviewed 3012346;
 ---------------------------------------------------------------------------------------------
 
 --(ii.9). Xem 3 năm có số bài báo mà mình đã phản biện nhiều nhất.
@@ -236,22 +283,34 @@ BEGIN
 		GROUP BY DATEPART(YEAR, AssignDate)
 		) AS NumOfArc Order by NumberOfArticle desc
 END
-EXEC get3YearsHaveTopArticleReviewed @reviewssn='1';
-
+GO
+EXEC get3YearsHaveTopArticleReviewed 3012346;
 ---------------------------------------------------------------------------------------------
 
 --(ii.10). Xem 3 bài báo mà mình đã phản biện có kết quả tốt nhất (acceptance).
 CREATE PROCEDURE ViewArticleReviewedBestResult (@ReviewerSsn VARCHAR (15))
 AS
-SELECT TOP 3 * FROM ARTICLE WHERE ID IN (SELECT ArticleID FROM EVALUATE WHERE Result = '3' AND ReviewerSsn = @ReviewerSsn)
-EXEC ViewArticleReviewedBestResult 111111111;
+	SELECT TOP 3 * 
+	FROM ARTICLE 
+	WHERE ID IN (SELECT ArticleID FROM EVALUATE WHERE ReviewerSsn = '3012345') AND Result = 3;
+GO
+
+select * from ARTICLE ORDER BY status desc
+select * from EVALUATE
+EXEC ViewArticleReviewedBestResult 3012345;
 ---------------------------------------------------------------------------------------------
 
 --(ii.11). Xem 3 bài báo mà mình đã phản biện có kết quả thấp nhất (rejection).
+
 CREATE PROCEDURE ViewArticleReviewedWorstResult (@ReviewerSsn VARCHAR (15))
 AS
-SELECT TOP 3 * FROM ARTICLE WHERE ID IN (SELECT ArticleID FROM EVALUATE WHERE Result = '0' AND ReviewerSsn = @ReviewerSsn)
-EXEC ViewArticleReviewedWorstResult 111111111;
+	SELECT TOP 3 * 
+	FROM ARTICLE 
+	WHERE ID IN (SELECT ArticleID FROM EVALUATE WHERE ReviewerSsn = '3012345') AND Result = 0;
+GO
+select * from ARTICLE ORDER BY result asc
+select * from EVALUATE
+EXEC ViewArticleReviewedWorstResult 3012351;
 ---------------------------------------------------------------------------------------------
 
 --(ii.12). Xem trung bình số bài báo mỗi năm mà mình đã phản biện trong 5 năm gần đây nhất.
@@ -268,8 +327,8 @@ BEGIN
 	) as NumOfArc;
 	
 END
-EXEC getNumOfArticleReviewedin5Years @reviewssn = '1';
-
+GO
+EXEC getNumOfArticleReviewedin5Years 3012351;
 ---------------------------------------------------------------------------------------------------------------------------------
 
 -- (iii). Tác giả liên lạc
@@ -282,8 +341,9 @@ BEGIN
 	UPDATE FIELD SET FieldID=@field
 	WHERE Ssn = @ssn;
 END
-EXEC updateProfile @ssn='12', @email='evans@gmmail.com', @job='Streamer', @fname='Lewandoski', @lname = 'Uzumaki', @address = '127 East 55th Street, Midtown East, New York, US', @WorkPlace = 'NK Company', @field='Music';
-
+GO
+select * from SCIENTIST
+EXEC updateProfile @ssn='1012345', @email='evans@gmmail.com', @job='Streamer', @fname='Lewandoski', @lname = 'Uzumaki', @address = '127 East 55th Street, Midtown East, New York, US', @WorkPlace = 'NK Company', @field='Music';
 -- (iii.2). Cập nhật thông tin của một bài báo đang được nộp.
 
 CREATE PROCEDURE updateArticle (@id VARCHAR(15), @title TEXT, @summary TEXT, @articlefile VARCHAR(255))
@@ -292,20 +352,22 @@ BEGIN
 	UPDATE ARTICLE SET Title=@title,Summary=@summary ,ArticleFile=@articlefile 
 	WHERE ID = @id;
 END
-EXEC updateArticle @id='3', @title='Road to Ninja', @summary='Naruto and the leaf ninja drive off a group of White Zetsu posing as fallen Akatsuki members', @articlefile='RoadToNinja.pdf';
-
+GO
+select * from ARTICLE
+EXEC updateArticle @id='412345', @title='Road to Ninja', @summary='Naruto and the leaf ninja drive off a group of White Zetsu posing as fallen Akatsuki members', @articlefile='RoadToNinja.pdf';
 -- (iii.3). Xem thông tin các tác giả của một bài báo.
 CREATE PROCEDURE getAuthorInfoOfArticle(@id VARCHAR(15))
 AS
 BEGIN
 	SELECT *
 	FROM SCIENTIST JOIN FIELD ON SCIENTIST.Ssn = FIELD.Ssn
-	WHERE SCIENTIST.Ssn = (SELECT Ssn
+	WHERE SCIENTIST.Ssn IN (SELECT Ssn
 				 FROM WRITE
 				 WHERE ID=@id)
 END
-EXEC getAuthorInfoOfArticle @id='8';
+GO
 
+EXEC getAuthorInfoOfArticle @id='412345';
 -- (iii.4). Xem trạng thái của một bài báo
 
 CREATE PROCEDURE getStatusofArticle(@id  VARCHAR(15))
@@ -315,8 +377,8 @@ BEGIN
 	FROM ARTICLE
 	WHERE ID=@id
 END
-EXEC getStatusofArticle @id='3';
-
+GO
+EXEC getStatusofArticle @id='412345';
 -- (iii.5). Xem kết quả phản biện của một bài báo.
 
 CREATE PROCEDURE getResultofArticle (@id VARCHAR(15))
@@ -326,7 +388,10 @@ BEGIN
 	FROM EVALUATE JOIN ARTICLE ON ID = ArticleID
 	WHERE ArticleID=@id 
 END
-EXEC getResultofArticle @id='7';
+GO
+select * from EVALUATE
+select * from ARTICLE
+EXEC getResultofArticle @id='412346';
 -- (iii.6). Xem danh sách các bài báo trong một năm.
 
 CREATE PROCEDURE getListArticle(@ssn VARCHAR(15))
@@ -336,7 +401,9 @@ BEGIN
 	FROM ARTICLE
 	WHERE AuthorSSn=@ssn AND SentDate > DATEADD(year,-1,GETDATE())
 END
-EXEC getListArticle @ssn = '0';
+GO
+select * from ARTICLE
+EXEC getListArticle @ssn = '1012351';
 -- (iii.7). Xem danh sách các bài báo đã đăng trong một năm.
 
 CREATE PROCEDURE getListArticleAccepted(@ssn VARCHAR(15))
@@ -344,10 +411,12 @@ AS
 BEGIN
 	SELECT * 
 	FROM ARTICLE 
-	WHERE Result = '4' AND SentDate > DATEADD(year,-1,GETDATE()) AND AuthorSSn=@ssn
+	WHERE Status = 4 AND SentDate > DATEADD(year,-1,GETDATE()) AND AuthorSSn=@ssn
 END
-EXEC getListArticleAccepted @ssn = '0';
+GO
 
+select * from ARTICLE
+EXEC getListArticleAccepted @ssn = '1012355';
 -- (iii.8). Xem danh sách các bài báo đang được xuất bản.
 
 CREATE PROCEDURE getListArticlePublishing(@ssn VARCHAR(15))
@@ -357,8 +426,9 @@ BEGIN
 	FROM ARTICLE 
 	WHERE Status = 3 AND AuthorSSn=@ssn
 END
-EXEC getListArticlePublishing @ssn = '0';
-
+GO
+select * from ARTICLE
+EXEC getListArticlePublishing @ssn = '1012352';
 -- (iii.9). Xem danh sách các bài báo có kết quả thấp nhất (rejection).
 
 CREATE PROCEDURE getListArticleRejected(@ssn VARCHAR(15))
@@ -366,13 +436,15 @@ AS
 BEGIN
 	SELECT ArticleID, NoteForAU, Result
 	FROM EVALUATE JOIN ARTICLE ON ID = ArticleID
-	WHERE Result='0'
+	WHERE Result=0
 	AND ArticleID IN (SELECT ID 
 					  FROM ARTICLE
 					  WHERE AuthorSSn=@ssn)
 END
-EXEC getListArticleRejected @ssn='2';
-
+GO
+select * from ARTICLE
+select * from EVALUATE
+EXEC getListArticleRejected @ssn='1012352';
 -- (iii.10). Xem tổng số bài báo đã gởi tạp chí mỗi năm trong 5 năm gần đây nhất.
 CREATE PROCEDURE getNumArticleIn5Years(@ssn VARCHAR(15))
 AS
@@ -383,8 +455,8 @@ BEGIN
 	GROUP BY DATEPART(YEAR,SentDate)
 	ORDER BY DATEPART(YEAR,SentDate) DESC
 END
-EXEC getNumArticleIn5Years @ssn='2';
-
+GO
+EXEC getNumArticleIn5Years @ssn='1012345';
 -- (iii.11). Xem tổng số bài báo nghiên cứu được đăng mỗi năm trong 5 năm gần đây nhất.
 CREATE PROCEDURE getListResearchArticleAcceptedIn5Years(@ssn VARCHAR(15))
 AS
@@ -395,7 +467,10 @@ BEGIN
 	GROUP BY DATEPART(YEAR,SentDate)
 	ORDER BY DATEPART(YEAR,SentDate) DESC
 END
-EXEC getListResearchArticleAcceptedIn5Years @ssn='2';
+GO
+select * from RESEARCH
+select * from ARTICLE
+EXEC getListResearchArticleAcceptedIn5Years @ssn='1012356';
 -- (iii.12). Xem tổng số bài báo tổng quan được đăng mỗi năm trong 5 năm gần đây nhất.
 CREATE PROCEDURE getListOverviewArticleAcceptedIn5Years(@ssn VARCHAR(15))
 AS
@@ -406,4 +481,7 @@ BEGIN
 	GROUP BY DATEPART(YEAR,SentDate)
 	ORDER BY DATEPART(YEAR,SentDate) DESC
 END
-EXEC getListOverviewArticleAcceptedIn5Years @ssn='2';
+GO
+select * from OVERVIEW
+select * from ARTICLE
+EXEC getListOverviewArticleAcceptedIn5Years @ssn='1012345';
